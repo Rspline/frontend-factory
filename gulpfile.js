115cd7bde8f44ad63774c80591b3a778ec6139de
Sergio del Campo <sergio@sergioelmoreno.com>
2017-07-20 20:16:02 +0200

    added sass error logs and paths

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('watch', function() {
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(sass({includePaths: ['scss']}))
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
@@ -105,7 +106,6 @@ gulp.task('vendor', function() {
 })
 
 // Build Sequences
-// ---------------
 
 gulp.task('default', function(callback) {
   runSequence(['sass', 'mustache', 'browserSync'], 'watch',
